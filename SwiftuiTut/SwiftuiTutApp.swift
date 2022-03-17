42d148940e0825adc4ba74b7a951a2f49865323c
Adejuwon Ayodimeji <juwwarn28@gmail.com>
2022-03-17 10:01:49 +0100

    Initial Commit

---
+++ SwiftuiTut/SwiftuiTutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiTutApp.swift
+//  SwiftuiTut
+//
+//  Created by Ayodimeji Adejuwon on 17/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiTutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
